aedb0d547a43c2247956894a296d7379e1bd84cf
sadykov@Lilith <mattsadov@gmail.com>
2021-06-08 16:54:46 +1000

    PySerial
    - Added AMS startup
    - Appended Charger code
    + Visualsiations remaims

---
+++ AMS_Monitor.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# %% [markdown]
+# # # AMS Monitoring and logging script
+# Ensure this script is started before the AMS is powered on, as it must be 
+# ready to respond to the AMS's charging check request
+# %%
+import sys, glob
+import io
+import serial as sr
+import time
+
+# Data management
+import pandas as pd
+import numpy as np
+
+DEFAULT_BAUD    : int = 115200
+# %%
+def serial_ports() -> list[str]:
+    """ Lists serial port names
+    Taken from: https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = sr.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, sr.SerialException):
+            pass
+    return result
+
+# %%
+if __name__ == '__main__':
+    print("AMS Monitor Starting\n")
+    aports  : list[str] = serial_ports()
+    print("====================\n")
+    for i in range(0, len(aports)):
+        print("({}) - {}\n".format(i+1, aports[i]))
+    
+    port    : int = int(input("Please select a serial port: "))-1
+    ser     : sr  = sr.Serial(aports[port], DEFAULT_BAUD)
+    
+    # Configurate Line terminator - CR/LF (0x0D 0x0A) - EOL
+    s = io.TextIOWrapper(io.BufferedRWPair(ser, ser),newline="\r\n")
+    print("Connected to serial port, awaiting AMS wakeup sequence...\n")
+    print("Press Stop Loop button on figure to terminate\n")
+
+    #! Setup all plots for animations
+
+    iter : int = 0
+    jlfid = open('jsonLog.json', 'a')
+    jlfid.write('{\n')
+    tlfid = open('textLog.txt', 'a')
+
+    try:
+        while(1):
+            while ser.in_waiting():
+                data = readline(s)
+
+            # time.sleep(1)
+    except:
+        print('Exiting ...')
+        jlfid.write('}')
+        jlfid.close()
+        tlfid.close()
+        ser.close()
+        # s.close()
+
+# %%
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-QUTMS_PSU
+# QUTMS_PSU
+## Matlab
+Only MatLab full installation above 2020a were tested.
+## Python
+Serial library installationn
+```
+# Using PIP        
+pip install PySerial
+
+# Using conda
+
+conda install PySerial
+```
+
+Solve permission error by chmod 666 or a+rw to port. To solve Unix permission error
+permenately, add yourself to the group 'dialout'.
+```
+sudo usermod -a -G dialout $USER
+```
